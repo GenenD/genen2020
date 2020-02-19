baf6548cb9d597276c2657c5d36ff715e5adf215
GenenD <56121430+GenenD@users.noreply.github.com>
2020-02-19 17:55:32 +0300

    Update app.module.js

--- app/app.module.js
+++ app/app.module.js
@@ -4,6 +4,6 @@ angular.module('designApp', [
   'ngResource'
 ]).config(function ($mdThemingProvider) {
   $mdThemingProvider.theme('default')
-    .primaryPalette('deep-purple')
-    .accentPalette('teal');
+    .primaryPalette('teal')
+    .accentPalette('grey');
 });
